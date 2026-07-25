f69bcbcf5b7e858680d241f53fcf42cf393df1dc
yousif51811 <yousif51811@gmail.com>
2026-07-25 17:20:38 +0300

    Fix User.hpp structure

--- Headers/User.hpp
+++ Headers/User.hpp
@@ -1,7 +1,25 @@
+#pragma once
 #include <string>
+
 class User {
+private:
+    std::string username;
+    std::string passwordHash;
+    std::string secretSentence;
+
 public:
-    std::string Username;
-    std::string Password;
-    std::string SecretSentence;
+    User(std::string u, std::string p, std::string s)
+        : username(u), passwordHash(p), secretSentence(s) {}
+
+    const std::string& GetUsername() const {
+        return username;
+    }
+
+    const std::string& GetPasswordHash() const {
+        return passwordHash;
+    }
+
+    const std::string& GetSecretSentence() const {
+        return secretSentence;
+    }
 };
